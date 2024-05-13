d9aa13dd07c2e1fcf0a86af3271fb997f835b952
Cesar Aguilar <Blueavian9@gmail.com>
2024-05-13 13:00:05 -0700

    updated commit

--- text2speech.py
+++ text2speech.py
@@ -1,4 +1,5 @@
-# Python3 code goes here
+# Python3 code goes here Creating J.A.R.V.I.S. powered by GROQ and Python
+#  URL: https://www.youtube.com/watch?v=HQ-wAw88X1I
 import os
 from deepgram import DeepgramClient, SpeakOptions
 
@@ -9,11 +10,13 @@ def text2speech(text):
     try:
         SPEECH_OPTIONS = {"text": text}
         deepgram = DeepgramClient(api_key=os.getenv("DG_API_KEY"))
+        
         options = SpeakOptions(
             model="aura-asteria-en",
             encoding="linear16",
             container="wav"
         )
+
         response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
         return filename
     except Exception as e:
