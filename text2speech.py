07f77a2d4729d388acfb4cfd32c18dadeb8479f5
Cesar Aguilar <Blueavian9@gmail.com>
2024-05-13 11:52:33 -0700

    updated commit

---
+++ text2speech.py
@@ -0,0 +1,23 @@
+# Python3 code goes here
+import os
+from deepgram import DeepgramClient, SpeakOptions
+
+SPEAK_OPTIONS = {"text": "Hello, how can I help you today?"}
+filename = "output.wav"
+
+def text2speech(text):
+    try:
+        SPEECH_OPTIONS = {"text": text}
+        deepgram = DeepgramClient(api_key=os.getenv("DG_API_KEY"))
+        options = SpeakOptions(
+            model="aura-asteria-en",
+            encoding="linear16",
+            container="wav"
+        )
+        response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
+        return filename
+    except Exception as e:
+        print(f"Exception: {e}")
+
+if __name__ == "__main__":
+    text2speech("This is a test")
